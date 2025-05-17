3197ec77ccf1a9f4b5ea10e1549b15dd95b648d9
nesiv3 <83553350+nesiv3@users.noreply.github.com>
2025-05-17 09:36:33 -0500

    Ajustes Cache

--- infraestructure/redis_client.py
+++ infraestructure/redis_client.py
@@ -1,11 +1,20 @@
 import redis
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+REDIS_URL = os.getenv("REDIS_URL")
+REDIS_PORT = os.getenv("REDIS_PORT")
+
+REDIS_USERNAME:str = os.getenv("REDIS_NAME","").strip()
+REDIS_PASSWORD:str = str(os.getenv("REDIS_PASS").strip())
+print(REDIS_USERNAME)
 redis_client = redis.Redis(
-    host='redis-13803.c282.east-us-mz.azure.redns.redis-cloud.com',
-    port=13803,
+    host=REDIS_URL,
+    port=REDIS_PORT,
     decode_responses=True,
-    username="BikeWork_APP",
-    password="Mantenimiento123.*",
+    username=REDIS_USERNAME,
+    password=REDIS_PASSWORD,
 )
 
 success = redis_client.set('foo', 'bar')
